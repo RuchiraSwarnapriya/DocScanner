30b01d16818273c75ae60d7a72e318695773ca6a
Ruchira Swarnapriya <ruchira.swarnapriya8@gmail.com>
2020-06-13 19:26:27 +0530

    Text Detection Error fix

--- lib/screens/readerScreen.dart
+++ lib/screens/readerScreen.dart
@@ -49,66 +49,64 @@ class _ReaderScreenState extends State<ReaderScreen> {
         backgroundColor: Colors.transparent,
         iconTheme: IconThemeData(color: Colors.blue),
       ),
-      body: Container(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Flexible(
-              child: Container(
-                height: 300,
-                child: SingleChildScrollView(
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                        top: 20.0, left: 40.0, right: 40.0, bottom: 20.0),
-                    child:
-                        Image.file(File(widget.imageRoute), fit: BoxFit.cover),
-                  ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Flexible(
+            child: Container(
+              height: 300,
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                      top: 20.0, left: 40.0, right: 40.0, bottom: 20.0),
+                  child: Image.file(File(widget.imageRoute), fit: BoxFit.cover),
                 ),
               ),
             ),
-            SizedBox(
-              height: 10,
-            ),
-            Center(
-              child: FlatButton(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(18.0),
-                    side: BorderSide(color: Colors.blue)),
-                color: Colors.blue,
-                textColor: Colors.white,
-                padding: EdgeInsets.all(10.0),
-                onPressed: () async {
-                  pickImage();
-                },
-                child: Text(
-                  "Read".toUpperCase(),
-                  style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.bold),
-                ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Center(
+            child: FlatButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18.0),
+                  side: BorderSide(color: Colors.blue)),
+              color: Colors.blue,
+              textColor: Colors.white,
+              padding: EdgeInsets.all(10.0),
+              onPressed: () async {
+                pickImage();
+              },
+              child: Text(
+                "Read".toUpperCase(),
+                style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.bold),
               ),
             ),
-            SizedBox(
-              height: 10,
-            ),
-            Center(
-              child: text == ''
-                  ? Text('Text will display here')
-                  : Expanded(
-                      child: SingleChildScrollView(
-                        child: Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: SelectableText(
-                            text,
-                          ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Center(
+            child: text == ''
+                ? Text('Text will display here')
+                : Expanded(
+                    child: SingleChildScrollView(
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: SelectableText(
+                          text,
                         ),
                       ),
                     ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-          ],
-        ),
+                  ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+        ],
       ),
     );
   }
 }
+
